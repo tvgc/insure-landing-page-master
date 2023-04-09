0370ea809988d4b6913646d4eac57a813031ab80
tvgc <thales.v.gc@hotmail.com>
2023-04-09 12:36:53 -0300

    studying

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+const header = document.querySelector('.js-header');
+
+const toggleMenu = document.querySelector('.js-toggle-menu')
+
+toggleMenu.addEventListener('click', e => {
+    header.classList.toggle('is-active')
+})
+
+console.log('teste');
